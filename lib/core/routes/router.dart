1b556af2e5e4e8be95a7165b49a685bc3b180344
Praise650 <ayokanmi1275@gmail.com>
2026-03-03 16:31:36 +0100

    custom router implementation

--- lib/core/routes/router.dart
+++ lib/core/routes/router.dart
@@ -2,18 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../ui/layout/bottom_nav_layout.dart';
+import '../../ui/screens/dashboard/booking_history/booking_histories_screen.dart';
+import '../../ui/screens/dashboard/booking_history/booking_details_screen.dart';
+import '../../ui/screens/dashboard/package/package_booking_summary_screen.dart';
+import '../../ui/screens/dashboard/package/confirm_package_select_screen.dart';
+import '../../ui/screens/auth/verify_phone/verify_phone_number_screen.dart';
+import '../../ui/screens/auth/account_setup/enable_permission_screen.dart';
+import '../../ui/screens/dashboard/package/add_package_detail_screen.dart';
+import '../../ui/screens/dashboard/trip/trip_booking_summary_screen.dart';
 import '../../ui/screens/auth/account_setup/account_setup_screen.dart';
 import '../../ui/screens/auth/account_setup/email_setup_screen.dart';
-import '../../ui/screens/auth/account_setup/enable_permission_screen.dart';
-import '../../ui/screens/auth/login/login_screen.dart';
-import '../../ui/screens/auth/register/register_screen.dart';
-import '../../ui/screens/auth/verify_phone/verify_phone_number_screen.dart';
-import '../../ui/screens/dashboard/books/books_screen.dart';
-import '../../ui/screens/dashboard/packages/packages_screen.dart';
+import '../../ui/screens/dashboard/trip/available_trips_screen.dart';
+import '../../ui/screens/dashboard/trip/pay_for_trip_screen.dart';
+import '../../ui/screens/dashboard/package/packages_screen.dart';
+import '../../ui/screens/dashboard/trip/book_a_trip_screen.dart';
 import '../../ui/screens/dashboard/profile/profile_screen.dart';
-import '../../ui/screens/dashboard/trips/trips_screens.dart';
+import '../../ui/screens/auth/register/register_screen.dart';
 import '../../ui/screens/onboarding/onboarding_screen.dart';
+import '../../ui/screens/dashboard/trip/trips_screen.dart';
+import '../../ui/screens/auth/login/login_screen.dart';
 import '../../ui/screens/splash/splash_screen.dart';
+import '../models/ui/package_size_model.dart';
+import '../models/trip_model.dart';
 import 'routes.dart';
 
 final _rootNavigatorKey = GlobalKey<NavigatorState>();
@@ -77,7 +87,6 @@ final router = GoRouter(
       builder: (context, state) => const LoginScreen(),
     ),
 
-
     //Dashboard Layout Flow
     ShellRoute(
       navigatorKey: _shellNavigatorKey,
@@ -85,10 +94,10 @@ final router = GoRouter(
       builder: (context, state, child) => BottomNavLayout(child: child),
       routes: [
         GoRoute(
-          path: Paths.BOOK,
-          name: Routes.BOOK,
+          path: Paths.HOME,
+          name: Routes.HOME,
           parentNavigatorKey: _shellNavigatorKey,
-          builder: (context, state) => const BooksScreen(),
+          builder: (context, state) => const TripsScreen(),
         ),
         GoRoute(
           path: Paths.PACKAGES,
@@ -97,10 +106,10 @@ final router = GoRouter(
           builder: (context, state) => const PackagesScreen(),
         ),
         GoRoute(
-          path: Paths.TRIPS,
-          name: Routes.TRIPS,
+          path: Paths.BOOKINGHISTORY,
+          name: Routes.BOOKINGHISTORY,
           parentNavigatorKey: _shellNavigatorKey,
-          builder: (context, state) => const TripsScreen(),
+          builder: (context, state) => const BookingHistoriesScreen(),
         ),
         GoRoute(
           path: Paths.PROFILE,
@@ -111,12 +120,61 @@ final router = GoRouter(
       ],
     ),
 
-    // GoRoute(
-    //   parentNavigatorKey: _rootNavigatorKey,
-    //   path: Paths.FORGETPASSWORD,
-    //   name: Routes.FORGETPASSWORD,
-    //   builder: (context, state) => const ForgetPasswordScreen(),
-    // ),
+    //Trip Booking Route
+    GoRoute(
+      parentNavigatorKey: _rootNavigatorKey,
+      path: Paths.AVAILABLETRIPS,
+      name: Routes.AVAILABLETRIPS,
+      builder: (context, state) => const AvailableTripsScreen(),
+    ),
+    GoRoute(
+      parentNavigatorKey: _rootNavigatorKey,
+      path: Paths.BOOKATRIP,
+      name: Routes.BOOKATRIP,
+      builder: (context, state) => const BookATripScreen(),
+    ),
+    GoRoute(
+      parentNavigatorKey: _rootNavigatorKey,
+      path: Paths.PAYFORTRIP,
+      name: Routes.PAYFORTRIP,
+      builder: (context, state) => const PayForTripScreen(),
+    ),
+    GoRoute(
+      parentNavigatorKey: _rootNavigatorKey,
+      path: Paths.TRIPSUMMARY,
+      name: Routes.TRIPSUMMARY,
+      builder: (context, state) => const TripBookingSummaryScreen(),
+    ),
+
+    //Package Booking Route
+    GoRoute(
+      parentNavigatorKey: _rootNavigatorKey,
+      path: Paths.ADDPACKAGEDETAIL,
+      name: Routes.ADDPACKAGEDETAIL,
+      builder: (context, state) => const AddPackageDetailScreen(),
+    ),
+    GoRoute(
+      parentNavigatorKey: _rootNavigatorKey,
+      path: Paths.CONFIRMPACKAGEDETAIL,
+      name: Routes.CONFIRMPACKAGEDETAIL,
+      builder: (context, state) =>
+          ConfirmPackageSelectScreen(listItem: state.extra as PackageSizeModel),
+    ),
+    GoRoute(
+      parentNavigatorKey: _rootNavigatorKey,
+      path: Paths.PACKAGEBOOKINGSUMMARY,
+      name: Routes.PACKAGEBOOKINGSUMMARY,
+      builder: (context, state) => PackageBookingSummaryScreen(),
+    ),
+
+    //Booking history Route
+    GoRoute(
+      parentNavigatorKey: _rootNavigatorKey,
+      path: Paths.BOOKINGHISTORYDETAIL,
+      name: Routes.BOOKINGHISTORYDETAIL,
+      builder: (context, state) =>
+          BookingDetailsScreen(trip: state.extra as Trip),
+    ),
     // GoRoute(
     //   parentNavigatorKey: _rootNavigatorKey,
     //   path: Paths.SECURITYPIN,
@@ -189,247 +247,6 @@ final router = GoRouter(
     //   path: Paths.RECEIPT,
     //   name: Routes.RECEIPT,
     //   builder: (context, state) => TransactionReceipt(item: state.extra),
-    // ),
-    //
-    // //bills sub screens
-    // GoRoute(
-    //   path: Paths.AIRTIMEDETAILS,
-    //   name: Routes.AIRTIMEDETAILS,
-    //   parentNavigatorKey: _rootNavigatorKey,
-    //   builder: (context, state) => const AirtimeDetails(),
-    // ),
-    // GoRoute(
-    //   path: Paths.AIRTIMETODATADETAILS,
-    //   name: Routes.AIRTIMETODATADETAILS,
-    //   parentNavigatorKey: _rootNavigatorKey,
-    //   builder: (context, state) => const AirtimeToCash(),
-    // ),
-    // GoRoute(
-    //   path: Paths.USSDCODES,
-    //   name: Routes.USSDCODES,
-    //   parentNavigatorKey: _rootNavigatorKey,
-    //   builder: (context, state) => const UssdCodes(),
-    // ),
-    // GoRoute(
-    //   path: Paths.DATADETAILS,
-    //   name: Routes.DATADETAILS,
-    //   parentNavigatorKey: _rootNavigatorKey,
-    //   builder: (context, state) => const DataDetails(),
-    // ),
-    // GoRoute(
-    //   path: Paths.CABLEDETAILS,
-    //   name: Routes.CABLEDETAILS,
-    //   parentNavigatorKey: _rootNavigatorKey,
-    //   builder: (context, state) => const CableDetails(),
-    // ),
-    // GoRoute(
-    //   path: Paths.ELECTRICITYDETAILS,
-    //   name: Routes.ELECTRICITYDETAILS,
-    //   parentNavigatorKey: _rootNavigatorKey,
-    //   builder: (context, state) => const ElectricityDetails(),
-    // ),
-    // GoRoute(
-    //   path: Paths.EDUCATIONDETAILS,
-    //   name: Routes.EDUCATIONDETAILS,
-    //   parentNavigatorKey: _rootNavigatorKey,
-    //   builder: (context, state) => const EducationDetails(),
-    // ),
-    // GoRoute(
-    //   path: Paths.BULKSMSDETAILS,
-    //   name: Routes.BULKSMSDETAILS,
-    //   parentNavigatorKey: _rootNavigatorKey,
-    //   builder: (context, state) => const BulkSmsDetails(),
-    // ),
-    // GoRoute(
-    //   path: Paths.BILLSRESPONSE,
-    //   name: Routes.BILLSRESPONSE,
-    //   parentNavigatorKey: _rootNavigatorKey,
-    //   builder: (context, state) =>
-    //       BillsResponseScreen(item: state.extra as BillsResponse),
-    // ),
-    // //beneficiary
-    // GoRoute(
-    //   path: Paths.BENEFICIARY,
-    //   name: Routes.BENEFICIARY,
-    //   parentNavigatorKey: _rootNavigatorKey,
-    //   builder: (context, state) =>
-    //       BeneficiaryScreen(filter: state.extra as String),
-    // ),
-    //
-    // //wallet screens
-    // GoRoute(
-    //   parentNavigatorKey: _rootNavigatorKey,
-    //   path: Paths.FUNDINGMETHODS,
-    //   name: Routes.FUNDINGMETHODS,
-    //   builder: (context, state) => const FundingMethodScreen(),
-    // ),
-    // GoRoute(
-    //   parentNavigatorKey: _rootNavigatorKey,
-    //   path: Paths.USSDFUNDING,
-    //   name: Routes.USSDFUNDING,
-    //   builder: (context, state) => const UssdFundingScreen(),
-    // ),
-    // GoRoute(
-    //   parentNavigatorKey: _rootNavigatorKey,
-    //   path: Paths.CARDFUNDING,
-    //   name: Routes.CARDFUNDING,
-    //   builder: (context, state) => const CardFundingScreen(),
-    // ),
-    // GoRoute(
-    //   parentNavigatorKey: _rootNavigatorKey,
-    //   path: Paths.MANUALFUNDING,
-    //   name: Routes.MANUALFUNDING,
-    //   builder: (context, state) => const ManualFundingScreen(),
-    // ),
-    // GoRoute(
-    //   parentNavigatorKey: _rootNavigatorKey,
-    //   path: Paths.WALLETHISTORY,
-    //   name: Routes.WALLETHISTORY,
-    //   builder: (context, state) => const WalletHistory(),
-    // ),
-    // GoRoute(
-    //   parentNavigatorKey: _rootNavigatorKey,
-    //   path: Paths.FILTERWALLETHISTORY,
-    //   name: Routes.FILTERWALLETHISTORY,
-    //   builder: (context, state) => const FilterWalletHistory(),
-    // ),
-    //
-    // // profile sub screens
-    // GoRoute(
-    //   path: Paths.VERIFYBVN,
-    //   name: Routes.VERIFYBVN,
-    //   parentNavigatorKey: _rootNavigatorKey,
-    //   builder: (context, state) => const VerifyBvnScreen(),
-    // ),
-    //
-    // GoRoute(
-    //   path: Paths.VERIFYID,
-    //   name: Routes.VERIFYID,
-    //   parentNavigatorKey: _rootNavigatorKey,
-    //   builder: (context, state) => const VerifyIdScreen(),
-    // ),
-    //
-    // GoRoute(
-    //   path: Paths.KYCSTATUS,
-    //   name: Routes.KYCSTATUS,
-    //   parentNavigatorKey: _rootNavigatorKey,
-    //   builder: (context, state) => const KycStatusScreen(),
-    // ),
-    //
-    // GoRoute(
-    //   path: Paths.PROFILESUBMENU,
-    //   name: Routes.PROFILESUBMENU,
-    //   parentNavigatorKey: _rootNavigatorKey,
-    //   builder: (context, state) =>
-    //       ProfileSubMenu(args: state.extra as ProfileSubMenuArgs),
-    // ),
-    //
-    // GoRoute(
-    //   path: Paths.REFERRAL,
-    //   name: Routes.REFERRAL,
-    //   parentNavigatorKey: _rootNavigatorKey,
-    //   builder: (context, state) =>
-    //       ReferralScreen(args: state.extra as ProfileSubMenuArgs),
-    // ),
-    //
-    // GoRoute(
-    //   path: Paths.SUPPORT,
-    //   name: Routes.SUPPORT,
-    //   parentNavigatorKey: _rootNavigatorKey,
-    //   builder: (context, state) =>
-    //       SupportScreen(args: state.extra as ProfileSubMenuArgs),
-    // ),
-    //
-    // //profile management screen
-    // GoRoute(
-    //   path: Paths.MANAGEACCOUNT,
-    //   name: Routes.MANAGEACCOUNT,
-    //   parentNavigatorKey: _rootNavigatorKey,
-    //   builder: (context, state) => const EditProfileScreen(),
-    // ),
-    // GoRoute(
-    //   path: Paths.SUREPLUGID,
-    //   name: Routes.SUREPLUGID,
-    //   parentNavigatorKey: _rootNavigatorKey,
-    //   builder: (context, state) => const CreateUserId(),
-    // ),
-    // //settings screens
-    // GoRoute(
-    //   path: Paths.SECURITY,
-    //   name: Routes.SECURITY,
-    //   parentNavigatorKey: _rootNavigatorKey,
-    //   builder: (context, state) => const SecuritySettings(),
-    // ),
-    // GoRoute(
-    //   parentNavigatorKey: _rootNavigatorKey,
-    //   path: Paths.CHANGEPASSWORD,
-    //   name: Routes.CHANGEPASSWORD,
-    //   builder: (context, state) => const ChangePasswordScreen(),
-    // ),
-    // GoRoute(
-    //   path: Paths.NOTIFICATIONSETTINGS,
-    //   name: Routes.NOTIFICATIONSETTINGS,
-    //   parentNavigatorKey: _rootNavigatorKey,
-    //   builder: (context, state) => const NotificationSettings(),
-    // ),
-    // GoRoute(
-    //   path: Paths.DELETEACCOUNT,
-    //   name: Routes.DELETEACCOUNT,
-    //   parentNavigatorKey: _rootNavigatorKey,
-    //   builder: (context, state) =>
-    //       EmptyStateScreen(args: state.extra as EmptyStateArgs?),
-    // ),
-    // //faq
-    // GoRoute(
-    //   path: Paths.FAQ,
-    //   name: Routes.FAQ,
-    //   parentNavigatorKey: _rootNavigatorKey,
-    //   builder: (context, state) => const FaqScreen(),
-    // ),
-    // GoRoute(
-    //   path: Paths.FAQDETAILS,
-    //   name: Routes.FAQDETAILS,
-    //   parentNavigatorKey: _rootNavigatorKey,
-    //   builder: (context, state) =>
-    //       FaqDetailsScreen(id: state.extra as FaqEntity),
-    // ),
-    //
-    // GoRoute(
-    //   path: Paths.WEBVIEW,
-    //   name: Routes.WEBVIEW,
-    //   parentNavigatorKey: _rootNavigatorKey,
-    //   builder: (context, state) => WebViewScreen(url: state.extra as String),
-    // ),
-    // GoRoute(
-    //   path: Paths.VERIFYACCOUNT,
-    //   name: Routes.VERIFYACCOUNT,
-    //   builder: (context, state) => const VerifyAccountScreen(),
-    // ),
-    // GoRoute(
-    //   path: Paths.NOTIFICATIONS,
-    //   name: Routes.NOTIFICATIONS,
-    //   builder: (context, state) => const NotificationScreen(),
-    // ),
-    // GoRoute(
-    //   path: Paths.UNAVAILABLE,
-    //   name: Routes.UNAVAILABLE,
-    //   builder: (context, state) => const UnauthorizedBottomSheet(),
-    // ),
-    // GoRoute(
-    //   path: Paths.PAYMENTSUCCESS,
-    //   name: Routes.PAYMENTSUCCESS,
-    //   onExit: (context, _) async => false,
-    //   builder: (context, state) => PaymentSuccess(
-    //     args: state.extra as PaymentStatusArgs,
-    //   ),
-    // ),
-    // GoRoute(
-    //   path: Paths.PAYMENTFAILED,
-    //   name: Routes.PAYMENTFAILED,
-    //   onExit: (context, _) async => false,
-    //   builder: (context, state) => PaymentFailed(
-    //     args: state.extra as PaymentStatusArgs,
-    //   ),
     // ),
   ],
 );
