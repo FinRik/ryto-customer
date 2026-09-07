cf16bf7da0b04846f8b1d9e1e31989237afbaf09
Praise650 <ayokanmi1275@gmail.com>
2026-09-07 03:43:12 +0100

    Updated app version

--- lib/ui/screens/dashboard/profile/help_support_screen.dart
+++ lib/ui/screens/dashboard/profile/help_support_screen.dart
@@ -153,7 +153,7 @@ class HelpSupportScreen extends StatelessWidget {
             const SizedBox(height: 40),
             const Center(
               child: Text(
-                "APP VERSION 1.0.0\n(4)",
+                "APP VERSION 1.0.0\n(10)",
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   color: Color(0xFF8F9BBA),
